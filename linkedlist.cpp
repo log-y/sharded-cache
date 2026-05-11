02b2a6506351a150c82a709e4efa38666ef692f2
logany <logan.yuan@gmail.com>
2026-05-11 16:01:47 -0400

    update: linked list finished

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -2,9 +2,61 @@
 
 #include "linkedlist.h"
 
-LinkedList::LinkedList(){
-  Node* head = new Node(-1, 0);
-  Node* tail = new Node(-1, 0);
+LinkedList::LinkedList()
+{
+  Node *head = new Node(-1, 0);
+  Node *tail = new Node(-1, 0);
   head->next = tail;
   tail->prev = head;
+  size = 0;
+}
+
+Node *LinkedList::get_back()
+{
+  if (size == 0)
+  {
+    return nullptr;
+  }
+  return tail->prev;
+}
+
+Node *LinkedList::get_front()
+{
+  if (size == 0)
+  {
+    return nullptr;
+  }
+  return head->next;
+}
+
+void LinkedList::add_between_nodes(Node *new_node, Node *left, Node *right)
+{
+  left->next = new_node;
+  new_node->prev = left;
+  right->prev = new_node;
+  new_node->next = right;
+}
+
+void LinkedList::push_back(Node *new_node)
+{
+  if (size == 0)
+  {
+    add_between_nodes(new_node, head, tail);
+  }
+
+  Node *last = tail->prev;
+
+  add_between_nodes(new_node, last, tail);
+}
+
+void LinkedList::push_front(Node *new_node)
+{
+  if (size == 0)
+  {
+    add_between_nodes(new_node, head, tail);
+  }
+
+  Node *front = tail->prev;
+
+  add_between_nodes(new_node, head, front);
 }
--- linkedlist.h
+++ linkedlist.h
@@ -9,9 +9,16 @@ class LinkedList {
   private:
     Node* head;
     Node* tail;
+    int size;
+
+    void add_between_nodes(Node* new_node, Node* left, Node* right);
 
   public:
     LinkedList();
+    void push_back(Node* new_node);
+    Node* get_back();
+    void push_front(Node* new_node);
+    Node* get_front();
 };
 
 #endif 
