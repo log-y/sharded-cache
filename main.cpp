abdad065ecba03396f58ee1cc0b2e6ec20adccab
logany <logan.yuan@gmail.com>
2026-05-12 16:58:40 -0400

    update: finished multi-sharding cache

--- main.cpp
+++ main.cpp
@@ -1,18 +1,106 @@
+// main.cpp
+
 #include <iostream>
 #include <string>
 #include <assert.h>
+#include <thread>
+#include <chrono>
 
 #include "cache.h"
 
 using namespace std;
 
-void run_basic_tests(){
+int OPS_PER_THREAD = 100000;
+int THREADS = 16;
+int NUM_SHARDS = 1000;
+int CAPACITY_PER_SHARD = 10000;
+
+void run_basic_tests();
+void basic_cache_tests();
+void stress_test_helper(Cache &cache, int num_operations, int thread_id);
+void stress_test();
+void stress_test_single_threaded();
+
+int main(int argv, char *argc[])
+{
+  run_basic_tests();
+  basic_cache_tests();
+
+  auto start_single = chrono::high_resolution_clock::now();
+  stress_test_single_threaded();
+  auto end_single = chrono::high_resolution_clock::now();
+  
+  auto start_multi = chrono::high_resolution_clock::now();
+  stress_test();
+  auto end_multi = chrono::high_resolution_clock::now();
+
+  auto duration_single = chrono::duration_cast<chrono::milliseconds>(end_single - start_single);
+  auto duration_multi = chrono::duration_cast<chrono::milliseconds>(end_multi - start_multi);
+
+  cout << "Time for single threaded ops: " << duration_single.count() << "ms" << endl;
+  cout << "Time for multi threaded ops: " << duration_multi.count() << "ms" << endl;
+
+  return 0;
+}
+
+void stress_test_single_threaded()
+{
+  Shard shard(NUM_SHARDS * CAPACITY_PER_SHARD);
+
+  cout << "starting single threaded stress test" << endl;
+
+  for (int i = 0; i < OPS_PER_THREAD * THREADS; i++)
+  {
+    int key = i;
+    int val = i + (key * 1000);
+
+    shard.set(key, val);
+    int retrieved = shard.get(key);
+  }
+
+  cout << "ending single threaded stress test" << endl;
+}
+
+void stress_test()
+{
+  Cache cache(NUM_SHARDS, CAPACITY_PER_SHARD);
+  vector<thread> workers;
+
+  cout << "starting stress test" << endl;
+
+  for (int i = 0; i < THREADS; i++)
+  {
+    workers.emplace_back(stress_test_helper, ref(cache), OPS_PER_THREAD, i);
+  }
+
+  for (auto &t : workers)
+  {
+    t.join();
+  }
+
+  cout << "ending stress test" << endl;
+}
+
+void stress_test_helper(Cache &cache, int num_operations, int thread_id)
+{
+  for (int i = 0; i < num_operations; i++)
+  {
+    int key = i;
+    int val = i + (thread_id * 1000);
+
+    cache.set(key, val);
+    int retrieved = cache.get(key);
+  }
+}
+
+void run_basic_tests()
+{
   Shard shard = Shard(2);
 
   shard.set(1, 10);
   shard.set(2, 20);
 
-  assert(shard.get(1) == 10); 
+  assert(shard.get(1) == 10);
   assert(shard.get(2) == 20);
 
   shard.set(3, 30);
@@ -30,13 +118,14 @@ void run_basic_tests(){
   cout << "basic tests are successful!" << endl;
 }
 
-void basic_cache_tests(){
+void basic_cache_tests()
+{
   Cache cache = Cache(1, 2);
 
   cache.set(1, 10);
   cache.set(2, 20);
 
-  assert(cache.get(1) == 10); 
+  assert(cache.get(1) == 10);
   assert(cache.get(2) == 20);
 
   cache.set(3, 30);
@@ -52,13 +141,4 @@ void basic_cache_tests(){
   assert(cache.get(4) == 40);
 
   cout << "basic cache tests are successful!" << endl;
-}
-
-
-int main(int argv, char *argc[])
-{
-  run_basic_tests();
-  basic_cache_tests();
-
-  return 0;
 }
